bb0401d57030b32372403cdaef0f41008320b157
Mathesh-571 <matheshkrishna571@gmail.com>
2022-07-07 14:34:40 +0530

    added README.md

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Loga Priya', 'lp', 'lp000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Sowmya Sree', 'ss', 'ss000', 'Project-2');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (3, 'Nishitha Shetty', 'ns', 'ns000', 'Project-3');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (4, 'Mahesh', 'mm', 'mm000', 'Project-4');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (5, 'Amit Jadhav', 'aj', 'aj000', 'Project-5');
